2caf0dc9ab567b2257d56b1e7a97d66b59f15ae3
Ketan Mayer-Patel <kmp@cs.unc.edu>
2020-02-18 13:54:33 -0500

    ArraySortedList solution

--- src/lec12/ex1/Main.java
+++ src/lec12/ex1/Main.java
@@ -3,10 +3,14 @@ package lec12.ex1;
 import java.util.ArrayList;
 import java.util.List;
 
+import lec12.sortedlist.SortedList;
+import lec12.sortedlist.ArraySortedList;
+
 public class Main {
 
 	public static void main(String[] args) {
 		List<Person> people = new ArrayList<Person>();
+
 		for (int i=0; i<10; i++) {
 			people.add(new RandomPerson());
 		}
--- src/lec12/sortedlist/SortedList.java
+++ src/lec12/sortedlist/SortedList.java
@@ -1,11 +1,48 @@
 package lec12.sortedlist;
 
-public interface SortedList<T> {
+public interface SortedList<T extends Comparable<T>> {
 
-	int look(T element);
-	void remove(int index);
+	void clear();
+	void add(T element);
+	T remove(int index);
 	T get(int index);
-	int find(T element);
+	default int find(T element) {
+		int idx = look(element);
+		return (get(idx).compareTo(element) == 0) ? idx : -1;
+	}
+	default int look(T element) {
+		if (isEmpty()) {
+			return 0;
+		}
+		
+		int low = 0;
+		int high = size()-1;
+
+		if (element.compareTo(get(low)) < 0) {
+			return 0;
+		}
+		if (element.compareTo(get(high)) > 0) {
+			return size();
+		}
+		while (low < high) {
+			int middle = (low+high) / 2;
+			T middle_element = get(middle);
+			if (element.compareTo(middle_element) > 0) {
+				low = middle+1;
+			} else if (element.compareTo(middle_element) < 0) {
+				high = middle;
+			} else {
+				while (middle > 0 && element.compareTo(get(middle-1)) == 0) {
+					middle--;
+				}
+				return middle;
+			}
+		}
+		// low == high == where the element should go
+		return low;
+	}
 	int size();
-	boolean isEmpty();
+	default boolean isEmpty() {
+		return (size() == 0);
+	}
 }
