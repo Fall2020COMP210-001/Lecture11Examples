174a16cd4aa8cd59f611fe6f4c3a42916f2ec44c
Ketan Mayer-Patel <kmp@cs.unc.edu>
2020-02-18 12:48:05 -0500

    SortedList interface

--- src/lec12/sortedlist/SortedList.java
+++ src/lec12/sortedlist/SortedList.java
@@ -2,4 +2,10 @@ package lec12.sortedlist;
 
 public interface SortedList<T> {
 
+	int look(T element);
+	void remove(int index);
+	T get(int index);
+	int find(T element);
+	int size();
+	boolean isEmpty();
 }
